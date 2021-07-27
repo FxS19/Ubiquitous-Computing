8d9a74db636bc689a45237182bb76fedfe655b2b
Felix Schmidt <felix.schmidt.1@gmx.de>
2021-07-27 21:03:52 +0200

    Driver requires SensorArray

--- driver.py
+++ driver.py
@@ -1,10 +1,13 @@
+from sensor import SensorArray
 from motor import Motor
 import time
 
 class Driver:
-    def __init__(self, motor_l: Motor, motor_r: Motor, alarm_sec: float) -> None:
+    """Functions for driving"""
+    def __init__(self, sensor_array: SensorArray, motor_l: Motor, motor_r: Motor, alarm_sec: float) -> None:
         self.motor_l = motor_l
         self.motor_r = motor_r
+        self.sensor_array = sensor_array
         self.__last_activated = time.monotonic()
         self.__alarm_sec = alarm_sec
     
