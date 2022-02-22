b683271fea1eafa8454033052d3e6b898ea3bc90
Felix Schmidt <felix.schmidt.1@gmx.de>
2022-02-22 20:44:40 +0100

    added settingstorage class

---
+++ settingStorage.py
@@ -0,0 +1,22 @@
+
+import os
+
+class SettingStorage:
+    def set_value(key: str, value: str) -> None:
+        """set value"""
+        if value:
+            with open('storage_' + key, 'wt+') as f:
+                f.write(value)
+                f.close()
+        else:
+            os.remove('storage_' + key)
+
+    def get_value(key: str) -> str:
+        """get value as string"""
+        try:
+            with open('storage_' + key, 'rt') as f:
+                v = f.read()
+                f.close()
+                return v
+        except OSError:
+            return '0'
