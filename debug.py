3efe8a8d54969120a57f883876ace06253bfb111
Felix Schmidt <felix.schmidt.1@gmx.de>
2022-03-13 20:34:38 +0100

    fix debug mode

--- debug.py
+++ debug.py
@@ -7,9 +7,9 @@ to start
 """
 import board
 import neopixel
-from driveByMatrix import Driver
+from driveByMatrix import DriveByMatrix
 
-driver = Driver("drive", neopixel.NeoPixel(board.NEOPIXEL, 1))
+driver = DriveByMatrix("drive", neopixel.NeoPixel(board.NEOPIXEL, 1))
 
 def start_update():
     """Perform sensor and motor updates in a loop"""
